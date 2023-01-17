804369989cdf6d5133ee460a4d438bc5b2920d93
Erik Persson <perssonerik7@gmail.com>
2023-01-17 16:11:11 +0100

    init

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import light from "./assets/light.jpg";
+
+function App() {
+  return (
+    <div className="App">
+      <nav>
+        <div>
+          <h1>socialChat</h1>
+          <img src={light} alt="light bulb" />
+        </div>
+      </nav>
+      <main>
+        <p>Tjabalala</p>
+      </main>
+    </div>
+  );
+}
+
+export default App;
